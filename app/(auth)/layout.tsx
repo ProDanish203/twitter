714fbd4863fff093004564eead8b9d9a69b1066c
Danish <danishsidd524@gmail.com>
2023-12-01 23:48:26 +0500

    favicon bug fix

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,8 +5,8 @@ import { redirect } from "next/navigation";
 import { getAuthSession } from '@/utils/auth';
 
 export const metadata: Metadata = {
-  title: 'Home | Twitter',
-  description: 'From breaking news and entertainment to sports and politics, get the full story with all the live commentary.',
+  title: 'X | Formerly Twitter',
+  description: 'X | A social media platform built by Danish Siddiqui, where users can share their daily life routines through tweets and also interact and engage with other users',
 }
 
 export default async function RootLayout({
@@ -22,6 +22,10 @@ export default async function RootLayout({
     <html lang="en">
       <head>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      <link rel="manifest" href="/site.webmanifest"/>
       </head>
       <body className='h-screen bg-black'>
         <AuthProvider>
