c4f4d403b4e8c220c5692faff620b555cf08114b
Danish <danishsidd524@gmail.com>
2025-07-23 23:03:23 +0500

    add: current user apis

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -9,8 +9,29 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { User, UserRole } from '@prisma/client';
+import { ApiProperty, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { Roles } from 'src/common/decorators/roles.decorator';
 
 @Controller('user')
+@ApiTags('User')
+@UseGuards(AuthGuard)
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Roles(...Object.values(UserRole))
+  @ApiProperty({ title: 'Get Current User' })
+  @Get('current')
+  async getCurrentUser(@CurrentUser() user: User) {
+    return user;
+  }
+
+  @Get('profile')
+  @Roles(...Object.values(UserRole))
+  @ApiProperty({ title: 'Get User Profile' })
+  async getProfile(@CurrentUser() user: User) {
+    return this.userService.getProfile(user);
+  }
 }
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,6 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { PrismaService } from 'src/common/services/prisma.service';
 import { StorageService } from 'src/common/services/storage.service';
+import { ApiResponse } from 'src/common/types/types';
+import { throwError } from 'src/common/utils/helpers';
 
 @Injectable()
 export class UserService {
@@ -8,4 +11,39 @@ export class UserService {
     private readonly prismaService: PrismaService,
     private readonly storageService: StorageService,
   ) {}
+
+  async getProfile(
+    user: User,
+  ): Promise<ApiResponse<Omit<User, 'password' | 'salt'>>> {
+    try {
+      const userProfile = await this.prismaService.user.findUnique({
+        where: { id: user.id },
+        omit: {
+          password: true,
+          salt: true,
+        },
+      });
+
+      return {
+        message: 'User profile retrieved successfully',
+        success: true,
+        data: userProfile,
+      };
+    } catch (err) {
+      throw throwError(
+        err.message || 'Failed to retrieve user profile',
+        err.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async getUserProfile(userId: string) {
+    try {
+    } catch (err) {
+      throw throwError(
+        err.message || 'Failed to retrieve user profile',
+        err.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
 }
