5cb10b99ace7c292e0661035127860baf8696881
Danish <danishsidd524@gmail.com>
2025-09-03 20:50:54 +0500

    add: basic auth ui styles

--- client/app/(auth)/(login)/page.tsx
+++ client/app/(auth)/(login)/page.tsx
@@ -1,15 +1,27 @@
-import React from "react";
 import { BottomLinks } from "./_components/bottom-links";
+import Image from "next/image";
 
 const AuthScreen = () => {
   return (
-    <main className="min-h-screen">
-      <div className="flex items-center justify-center">
-        <div></div>
-        <div></div>
+    <main className="relative min-h-screen w-screen flex flex-col items-center justify-between">
+      <div className="max-sm:container max-sm:mx-auto flex flex-1 w-full h-full divide-red-500 divide-x-2">
+        <div className="flex items-center justify-center w-full">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="X Logo"
+            width={250}
+            height={250}
+            className="object-contain"
+          />
+        </div>
+        <div className="p-4 w-full flex flex-col justify-center">
+          <div className="p-5">
+            <h2 className="my-12 text-6xl font-bold">Happening now</h2>
+            <p className="mb-8 text-3xl font-bold">Join today.</p>
+          </div>
+        </div>
       </div>
 
-      {/* Links */}
       <BottomLinks />
     </main>
   );
