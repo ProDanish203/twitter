1593dc3254bbc0be8c10bee702e18c8e158dfebd
Danish <danishsidd524@gmail.com>
2025-09-07 16:11:36 +0500

    fix: styles

--- client/app/(auth)/(login)/flow/login/page.tsx
+++ client/app/(auth)/(login)/flow/login/page.tsx
@@ -1,10 +1,16 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { LoginForm } from "@/app/(auth)/(login)/_components/login-form";
+import { useState } from "react";
+import { PasswordLoginForm } from "../../_components/password-login-form";
+import { cn } from "@/lib/utils";
 
 const LoginScreen = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
   return (
     <div className="bg-primary/5 backdrop-blur-sm flex items-center justify-center p-4 min-h-screen">
       <div className="relative md:w-xl max-h-[90vh] rounded-lg shadow-xl overflow-hidden bg-background">
@@ -28,8 +34,11 @@ const LoginScreen = () => {
           <div />
         </div>
 
-        <div className="pb-16">
-          <LoginForm title="Sign in to X" />
+        <div className={cn(activeTab === 0 ? "pb-16" : "")}>
+          {activeTab === 0 && (
+            <LoginForm title="Sign in to X" setActiveTab={setActiveTab} />
+          )}
+          {activeTab === 1 && <PasswordLoginForm />}
         </div>
       </div>
     </div>
