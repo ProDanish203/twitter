2ce3b31b0ecb1af5934494aab797c519bc7da41a
Danish <danishsidd524@gmail.com>
2025-09-09 23:42:37 +0500

    fix: input styles

--- client/components/form/floating-input.tsx
+++ client/components/form/floating-input.tsx
@@ -27,7 +27,7 @@ export const FloatingInput = <T extends FieldValues>({
         type={type}
         placeholder={""}
         className={cn(
-          `z-[1] font-roboto relative px-2.5 pb-2.5 pt-2.5 w-full text-sm text-lightGray bg-transparent rounded-xs border border-primary dark:border-neutral-800 appearance-none focus:outline-none focus:ring-0 focus:border-primary dark:focus:border-primary peer`,
+          `z-[1] font-roboto relative px-2.5 pb-2.5 pt-6 w-full text-sm bg-transparent rounded-xs border border-neutral-800 dark:border-neutral-800 appearance-none focus:outline-none focus:ring-0 focus:border-primary dark:focus:border-primary peer disabled:bg-neutral-700/40 disabled:opacity-65`,
           isError && "!border-red-500/60 !focus:border-red-500/60"
         )}
         {...register(name as Path<T>)}
@@ -35,7 +35,7 @@ export const FloatingInput = <T extends FieldValues>({
       <label
         htmlFor={name}
         className={cn(
-          `z-[1] font-roboto absolute text-sm text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 origin-[0] bg-bg dark:bg-background px-2 peer-focus:px-2 peer-focus:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1 user-select-none pointer-events-none`,
+          `font-roboto absolute text-sm text-gray-400 duration-200 transform top-4 left-2.5 origin-[0] peer-focus:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:top-4 peer-focus:top-1.5 peer-focus:scale-100 peer-focus:text-xs peer-[&:not(:placeholder-shown)]:top-1.5 peer-[&:not(:placeholder-shown)]:scale-95 peer-[&:not(:placeholder-shown)]:text-xs rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto user-select-none pointer-events-none transition-all`,
           isError && "text-red-500/80 peer-focus:text-red-500/80",
           className
         )}
--- client/components/form/password-input.tsx
+++ client/components/form/password-input.tsx
@@ -29,7 +29,7 @@ export const PasswordInput = <T extends FieldValues>({
         type={showPass ? "text" : "password"}
         id={name}
         className={cn(
-          "block px-2.5 pb-2.5 pt-2.5 w-full text-sm text-lightGray bg-transparent rounded-xs border border-border dark:border-neutral-800 appearance-none focus:outline-none focus:ring-0 focus:border-primary peer",
+          "z-[1] font-roboto relative px-2.5 pb-2.5 pt-6 pr-12 w-full text-sm bg-transparent rounded-xs border border-border dark:border-neutral-800 appearance-none focus:outline-none focus:ring-0 focus:border-primary dark:focus:border-primary peer disabled:bg-neutral-700/40 disabled:opacity-65",
           isError && "!border-red-500/60 !focus:border-red-500/60"
         )}
         placeholder=""
@@ -38,7 +38,7 @@ export const PasswordInput = <T extends FieldValues>({
       <label
         htmlFor={name}
         className={cn(
-          "absolute text-sm text-gray-400  duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-background dark:bg-background  px-2 peer-focus:px-2 peer-focus:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1",
+          "font-roboto absolute text-sm text-gray-400 duration-200 transform top-4 left-2.5 origin-[0] peer-focus:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:top-4 peer-focus:top-1.5 peer-focus:scale-95 peer-focus:text-xs peer-[&:not(:placeholder-shown)]:top-1.5 peer-[&:not(:placeholder-shown)]:scale-95 peer-[&:not(:placeholder-shown)]:text-xs rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto user-select-none pointer-events-none transition-all",
           isError && "text-red-500/80 peer-focus:text-red-500/80",
           className
         )}
@@ -47,10 +47,14 @@ export const PasswordInput = <T extends FieldValues>({
       </label>
       <button
         type="button"
-        className="absolute right-3 top-3 cursor-pointer"
+        className="absolute right-3 top-4 cursor-pointer z-[2]"
         onClick={() => setShowPass((prev) => !prev)}
       >
-        {!showPass ? <Eye className="size-5" /> : <EyeOff className="size-5" />}
+        {!showPass ? (
+          <Eye className="size-5 text-gray-400" />
+        ) : (
+          <EyeOff className="size-5 text-gray-400" />
+        )}
       </button>
     </div>
   );
