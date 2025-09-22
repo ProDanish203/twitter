91a8df726eb73ff9130c87c550ec6ae20ea7b836
Danish <danishsidd524@gmail.com>
2025-09-22 22:30:13 +0500

    add: home tabs

--- client/app/(root)/home/page.tsx
+++ client/app/(root)/home/page.tsx
@@ -1,30 +1,53 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
+const homeTabs = ["For you", "Following"];
 
 const HomeScreen = () => {
+  const [activeTab, setActiveTab] = useState<(typeof homeTabs)[number]>(
+    homeTabs[0]
+  );
+
   return (
     <div className="">
       {/* Header */}
       <div className="sticky top-0 bg-black/80 backdrop-blur-sm border-b border-neutral-800 z-10">
         <div className="relative w-full mx-auto flex items-center justify-between">
-          <div className="w-full">
-            <Button
-              variant="tab"
-              role="button"
-              className="w-full p-0 h-16 rounded-none cursor-pointer"
-            >
-              <span className="text-[16px] font-semibold">For you</span>
-            </Button>
-          </div>
+          {homeTabs.map((tab) => {
+            const isActive = activeTab === tab;
 
-          <div className="w-full">
-            <Button
-              variant="tab"
-              role="button"
-              className="w-full h-16 rounded-none cursor-pointer"
-            >
-              <span className="text-[16px] font-semibold">Following</span>
-            </Button>
-          </div>
+            return (
+              <div className="w-full">
+                <Button
+                  variant="tab"
+                  role="button"
+                  className={cn(
+                    "w-full p-0 h-16 rounded-none cursor-pointer",
+                    isActive
+                      ? "text-white hover:text-white"
+                      : "text-neutral-500 hover:text-neutral-500"
+                  )}
+                  onClick={() => setActiveTab(tab)}
+                >
+                  <div className="relative h-full flex items-center justify-center">
+                    <span
+                      className={cn(
+                        "text-[16px]",
+                        isActive ? "font-semibold" : "font-medium"
+                      )}
+                    >
+                      {tab}
+                    </span>
+                    {isActive && (
+                      <span className="absolute bottom-0 left-0 h-1 w-full bg-primary rounded-full"></span>
+                    )}
+                  </div>
+                </Button>
+              </div>
+            );
+          })}
         </div>
       </div>
 
